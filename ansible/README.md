f74d54cd5fdcefe1cc41569eb7342aa859e8de43
Jesse Plymale <jesse@jesseplymale.com>
2021-05-03 21:00:54 -0500

    Initial checkin

---
+++ ansible/README.md
@@ -0,0 +1,11 @@
+# Running Ansible on the Raspberry Pi 4 8gb
+
+```bash
+sudo apt install python3
+
+# Use pip to install Ansible
+python3 -m pip install --user ansible
+python3 -m pip install "ansible-lint[community,yamllint]" 
+
+# 
+```
---
+++ ansible/files/dot_bashrc_extras.sh
@@ -0,0 +1,17 @@
+
+# Used to add extra entries to the path
+pathmunge () {
+    case ":${PATH}:" in
+        *:"$1":*)
+            ;;
+        *)
+            if [ "$2" = "after" ] ; then
+                PATH=$PATH:$1
+            else
+                PATH=$1:$PATH
+            fi
+    esac
+}
+
+# Add extra directories to path
+pathmunge ${HOME}/.local/bin
